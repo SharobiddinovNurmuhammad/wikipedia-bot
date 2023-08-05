620ddf256f99f2e6bcef83b03140b5d0b697530f
SharobiddinovNurmuhammad <sharobiddinovnurmuhammad23@gmail.com>
2023-08-05 14:29:46 +0500

    intial commit

--- app.py
+++ app.py
@@ -12,19 +12,28 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+# /start commandasi uchun handlar
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    text = f"Assalomu alaykum <b>{message.from_user.full_name}</b>, Wikisearch botga xush kelibsiz!\nBotdan qanday foydalanishni bilish uchun /help buyrug'ini kiriting!"
+    await message.reply(text=text, parse_mode='HTML')
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.", parse_mode='HTML')
-
+# /help commadasi uchun handlar
+@dp.message_handler(commands='help')
+async def help(message: types.Message):
+    text = "Wikisearch dan foydalanish uchun botga maqolangizga oid biror kalit so'z yuboring:\n<b>Misol uchun:</b>\nPython\nSalom\nKitob va h.k.lar.. "
+    await message.reply(text=text, parse_mode='HTML')
 
 @dp.message_handler()
 async def wikibot(message: types.Message):
     msg = message.text
     arr = await wikisearch(msg)
     text = await wikiloops(arr)
-    await message.answer(text=text, reply_markup=wikisearchbtn(arr), parse_mode='HTML')
-
+    if len(wikisearchbtn(arr)['inline_keyboard']) == 0:
+        await message.answer("Bunday kalit so'zga oid maqola topilmadi!")
+    else:
+        await message.answer(text=text, reply_markup=wikisearchbtn(arr), parse_mode='HTML')
+        
 
 @dp.callback_query_handler()
 async def wikipages(call: types.CallbackQuery):
